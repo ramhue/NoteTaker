8b2aec675be0428084ec18495286f181181b812e
CampusUser <campususer@csufmac-009.local>
2019-03-18 23:38:12 -0700

    11:38 commit

---
+++ NoteTakerThingy/NoteTakerThingy/MyToDo.swift
@@ -0,0 +1,109 @@
+//
+//  MyToDo.swift
+//   NoteTakerThingy
+//
+//  Created by CampusUser on 3/9/19.
+//  Copyright © 2019 ramhue. All rights reserved.
+//
+
+
+import Foundation
+
+class ToDoItem: NSObject, NSCoding
+{
+	var title: String
+	var done: Bool
+
+	public init(title: String)
+	{
+		self.title = title
+		self.done = false
+	}
+
+	required init?(coder aDecoder: NSCoder)
+	{
+		if let title = aDecoder.decodeObject(forKey: "title") as? String
+		{
+			self.title = title
+		}
+		else
+		{
+						return nil
+		}
+
+		if aDecoder.containsValue(forKey: "done")
+		{
+			self.done = aDecoder.decodeBool(forKey: "done")
+		}
+		else
+		{
+			return nil
+		}
+	}
+
+	func encode(with aCoder: NSCoder)
+	{
+		aCoder.encode(self.title, forKey: "title")
+		aCoder.encode(self.done, forKey: "done")
+	}
+}
+
+extension ToDoItem
+{
+	public class func getMockData() -> [ToDoItem]
+	{
+		return [
+			ToDoItem(title: "Milk"),
+			ToDoItem(title: "Chocolate"),
+			ToDoItem(title: "Light bulb"),
+			ToDoItem(title: "Dog food")
+		]
+	}
+}
+
+extension Collection where Iterator.Element == ToDoItem
+{
+	private static func persistencePath() -> URL?
+	{
+		let url = try? FileManager.default.url(
+			for: .applicationSupportDirectory,
+			in: .userDomainMask,
+			appropriateFor: nil,
+			create: true)
+
+		return url?.appendingPathComponent("todoitems.bin")
+	}
+
+	func writeToPersistence() throws
+	{
+		if let url = Self.persistencePath(), let array = self as? NSArray
+		{
+			let data = NSKeyedArchiver.archivedData(withRootObject: array)
+			try data.write(to: url)
+		}
+		else
+		{
+			throw NSError(domain: "com.example.MyToDo", code: 10, userInfo: nil)
+		}
+	}
+
+	static func readFromPersistence() throws -> [ToDoItem]
+	{
+		if let url = persistencePath(), let data = (try Data(contentsOf: url) as Data?)
+		{
+			if let array = NSKeyedUnarchiver.unarchiveObject(with: data) as? [ToDoItem]
+            //if let array = NSKeyedUnarchiver.unarchivedObject(ofClasses: [ToDoItem], from: data)
+            {
+				return array
+			}
+			else
+			{
+				throw NSError(domain: "com.example.MyToDo", code: 11, userInfo: nil)
+			}
+		}
+		else
+		{
+			throw NSError(domain: "com.example.MyToDo", code: 12, userInfo: nil)
+		}
+	}
+}
---
+++ NoteTakerThingy/NoteTakerThingy/ViewController.swift
@@ -0,0 +1,159 @@
+//
+//  ViewController.swift
+//NoteTakerThingy
+//
+//  Created by CampusUser on 3/9/19.
+//  Copyright © 2019 ramhue. All rights reserved.
+//
+
+
+import UIKit
+
+class ViewController: UITableViewController
+{
+	private var todoItems = [ToDoItem]()
+
+	override func viewDidLoad()
+	{
+		super.viewDidLoad()
+
+		self.title = "To-Do"
+
+		self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(ViewController.didTapAddItemButton(_:)))
+
+		
+		NotificationCenter.default.addObserver(
+			self,
+			selector: #selector(UIApplicationDelegate.applicationDidEnterBackground(_:)),
+            name: UIApplication.didEnterBackgroundNotification,
+			object: nil)
+
+		do
+		{
+			self.todoItems = try [ToDoItem].readFromPersistence()
+		}
+		catch let error as NSError
+		{
+			if error.domain == NSCocoaErrorDomain && error.code == NSFileReadNoSuchFileError
+			{
+				NSLog("No persistence file found, not necesserially an error...")
+			}
+			else
+			{
+				let alert = UIAlertController(
+					title: "Error",
+					message: "Could not load the to-do items!",
+					preferredStyle: .alert)
+
+				alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+
+				self.present(alert, animated: true, completion: nil)
+
+				NSLog("Error loading from persistence: \(error)")
+			}
+		}
+	}
+
+	@objc
+	public func applicationDidEnterBackground(_ notification: NSNotification)
+	{
+		do
+		{
+			try todoItems.writeToPersistence()
+		}
+		catch let error
+		{
+			NSLog("Error writing to persistence: \(error)")
+		}
+	}
+
+	override func didReceiveMemoryWarning()
+	{
+		super.didReceiveMemoryWarning()
+	}
+
+    @objc func didTapAddItemButton(_ sender: UIBarButtonItem)
+	{
+		// Create an alert
+		let alert = UIAlertController(
+			title: "New to-do item",
+			message: "Insert the title of the new to-do item:",
+			preferredStyle: .alert)
+
+		alert.addTextField(configurationHandler: nil)
+
+		alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+
+		alert.addAction(UIAlertAction(title: "OK", style: .default, handler:
+		{ (_) in
+			// Get the title the user inserted, but only if it is not an empty string
+			if let title = alert.textFields?[0].text, title.characters.count > 0
+			{
+				self.addNewToDoItem(title: title)
+			}
+		}))
+
+		// Present the alert to the user
+		self.present(alert, animated: true, completion: nil)
+	}
+
+	private func addNewToDoItem(title: String)
+	{
+		// The index of the new item will be the current item count
+		let newIndex = todoItems.count
+
+		// Create new item and add it to the todo items list
+		todoItems.append(ToDoItem(title: title))
+
+		// Tell the table view a new row has been created
+		tableView.insertRows(at: [IndexPath(row: newIndex, section: 0)], with: .top)
+	}
+
+	override func numberOfSections(in tableView: UITableView) -> Int
+	{
+		return 1
+	}
+
+	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+	{
+		return todoItems.count
+	}
+
+	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+	{
+		let cell = tableView.dequeueReusableCell(withIdentifier: "cell_todo", for: indexPath)
+
+		if indexPath.row < todoItems.count
+		{
+			let item = todoItems[indexPath.row]
+			cell.textLabel?.text = item.title
+
+            let accessory: UITableViewCell.AccessoryType = item.done ? .checkmark : .none
+			cell.accessoryType = accessory
+		}
+
+		return cell
+	}
+
+	override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath)
+	{
+		tableView.deselectRow(at: indexPath, animated: true)
+
+		if indexPath.row < todoItems.count
+		{
+			let item = todoItems[indexPath.row]
+			item.done = !item.done
+
+			tableView.reloadRows(at: [indexPath], with: .automatic)
+		}
+	}
+
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath)
+	{
+		if indexPath.row < todoItems.count
+		{
+			todoItems.remove(at: indexPath.row)
+			tableView.deleteRows(at: [indexPath], with: .top)
+		}
+	}
+}
